eaa61a6b3411218f25cd40e03d0d1b40a1035cff
araheja <araheja@umich.edu>
2024-11-18 19:42:48 -0500

    donation bar changes

--- src/js/components/SchoolContainer.jsx
+++ src/js/components/SchoolContainer.jsx
@@ -11,14 +11,15 @@ const SchoolContainer = () => {
 	const [selectedSchool, setSelectedSchool] = useState('');
 	const [lanternMoney, setLanternMoney] = useState(0);
 	const [dailyMoney, setDailyMoney] = useState(0);
-
 	// TODO: Update the json to use 2024.json.
 	// The lambda should be changed as well.
 	useEffect(() => {
-		fetch('https://stash.michigandaily.com/rivalry-edition-donations/2024.json')
+		fetch('https://s3.us-east-2.amazonaws.com/stash.michigandaily.com/rivalry-edition-donations/2024.json')
 			.then((response) => response.json())
 			.then(({ lantern, daily }) => {
 				setLanternMoney(Number(lantern));
+				console.log(lantern)
+				console.log(daily)
 				setDailyMoney(Number(daily));
 			});
 	}, []);
